9b929f9353dbfde765c7162dad27cb020756f356
samiullah <sami606713@gmail.com>
2024-06-17 00:41:10 +0500

    Applying voting classifier

--- src/components/model_training.py
+++ src/components/model_training.py
@@ -3,7 +3,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from src.utils import model_evulation,save_report
+from src.utils import model_evulation,save_report,voting_classifier,find_top_models,save_file
 import os
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -33,15 +33,24 @@ def inisiate_model_training(train_array,test_array):
                                     min_impurity_decrease=0.01,
                                     random_state=43,          
                                     class_weight='balanced'),
-        "RF":RandomForestClassifier(),
+        "RF":RandomForestClassifier(class_weight='balanced'),
         "xgboost":XGBClassifier(),  
         "knn":KNeighborsClassifier(n_neighbors=5,n_jobs=-1)  
     }
 
     # Evulate the models
-    final_report=model_evulation(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,model_dic=models) 
-    print(final_report)
+    final_report:dict=model_evulation(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,model_dic=models) 
+    # print(final_report)
     save_report(file_path=report_path,report=final_report)
-    counter+=1
+    # counter+=1
+    sorted_model=find_top_models(final_report)
+    
+    logging.info("Call the voting classifier")
+    final_model,full_report_2=voting_classifier(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,model_dic=models,sorted_model=sorted_model)
+
+    logging.info(f"final model at this location {model_path}")
+    save_file(model_path,final_model)
+
+    print(full_report_2)
 
     
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import mlflow.sklearn
 import json
 import numpy as np
 from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import VotingClassifier
 from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score,confusion_matrix
 import os
 import logging
@@ -59,7 +60,7 @@ def model_evulation(x_train,y_train,x_test,y_test,model_dic):
     logging.info("Itrating on model dic")
     report={}
     for model_name,model in model_dic.items():
-        with mlflow.start_run(run_name=model_name):
+        # with mlflow.start_run(run_name=model_name):
             logging.info(f'fit the model {model_name}')
             model.fit(x_train,y_train)
 
@@ -75,14 +76,14 @@ def model_evulation(x_train,y_train,x_test,y_test,model_dic):
             logging.info(f'Final report of the model {model_name}')
             full_report=generate_report(actual=y_test,pre=pre)
 
-            # # Log parameters and metrics with MLflow
-            mlflow.log_param("model_name", model_name)
-            mlflow.log_metric("train_score", train_score)
-            mlflow.log_metric("test_score", test_score)
-            # mlflow.log_metrics(full_report)
+            # # # Log parameters and metrics with MLflow
+            # mlflow.log_param("model_name", model_name)
+            # mlflow.log_metric("train_score", train_score)
+            # mlflow.log_metric("test_score", test_score)
+            # # mlflow.log_metrics(full_report)
             
-            # # Log the model
-            mlflow.sklearn.log_model(model, model_name)
+            # # # Log the model
+            # mlflow.sklearn.log_model(model, model_name)
 
             report[model_name]={
                 "train_score":train_score,
@@ -93,4 +94,34 @@ def model_evulation(x_train,y_train,x_test,y_test,model_dic):
     return report
 
 
+def voting_classifier(x_train,y_train,x_test,y_test,model_dic,sorted_model):
+    report={}
+    estimator=[(model[0], model_dic[model[0]]) for model in sorted_model]
+    # print(estimator)
+    voting_clf = VotingClassifier(estimators=estimator,voting='soft')
+    logging.info("voting successfull")
+    voting_clf.fit(x_train,y_train)
 
+    pre=voting_clf.predict(x_test)
+
+    # logging.info(f'Cross Validation of the model {model_name} on training data')
+    train_score=cross_val_score(voting_clf,x_train,y_train,cv=5,scoring="accuracy",n_jobs=-1).mean()
+            
+    # logging.info(f'Cross Validation of the model {model_name} on testing data')
+    test_score=cross_val_score(voting_clf,x_test,y_test,cv=5,scoring="accuracy",n_jobs=-1).mean()
+            
+    # logging.info(f'Final report of the model {model_name}')
+    full_report=generate_report(actual=y_test,pre=pre)
+
+    report['voting_clf']={
+                "train_score":train_score,
+                "test_score":test_score,
+                "full_report":full_report
+            }
+
+    return [voting_clf,report]
+
+
+def find_top_models(report, top_n=3):
+    sorted_models = sorted(report.items(), key=lambda item: item[1]["full_report"]["Recall"], reverse=True)
+    return sorted_models[:top_n]
